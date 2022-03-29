bcf031302c6039d7f87d12dc90125443ba981a1b
Carlzon50 <72607945+Carlzon50@users.noreply.github.com>
2022-03-29 15:36:15 +0200

    Server up and running

--- client/index.jsx
+++ client/index.jsx
@@ -1,1 +1,4 @@
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Dennis Kuk</h1>, document.getElementById("app"));
